637ddfd1631e2dff5033af90afd6f9f8e6229346
117503445 <t117503445@gmail.com>
2025-01-13 13:17:38 +0800

    log
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- go.mod
+++ go.mod
@@ -3,10 +3,13 @@ module 117503445/nix-binary-cache
 go 1.23.4
 
 require (
-	github.com/117503445/goutils v0.0.0-20250111025412-c22ead8dc001 // indirect
+	github.com/117503445/goutils v0.0.0-20250113050529-9aa5fda316c3
+	github.com/rs/zerolog v1.33.0
+)
+
+require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/rs/zerolog v1.33.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
 )
--- main.go
+++ main.go
@@ -31,11 +31,15 @@ func newHandle(httpProxy string, upstreams []upstream) (func(w http.ResponseWrit
 	}
 
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Debug().Str("path", r.URL.Path).Msg("request")
+		ctx := r.Context()
+		reqID := goutils.UUID4()
+		ctx = log.With().Str("reqID", reqID).Logger().WithContext(ctx)
+
+		log.Ctx(ctx).Debug().Str("path", r.URL.Path).Str("method", r.Method).Msg("request")
 		// TODO 优先从缓存中读取。
 		cacheFilePath := cacheDir + fmt.Sprintf("%x", r.URL.String())
 		if _, err := os.Stat(cacheFilePath); err == nil {
-			log.Debug().Str("cacheFile", cacheFilePath).Msg("cache hit")	
+			log.Ctx(ctx).Debug().Str("cacheFile", cacheFilePath).Msg("cache hit")
 			// Cache hit: Serve the cached file
 			http.ServeFile(w, r, cacheFilePath)
 			return
@@ -43,7 +47,7 @@ func newHandle(httpProxy string, upstreams []upstream) (func(w http.ResponseWrit
 
 		// 尝试按顺序 upstream 中获取
 		var resp *http.Response
-		for i, u := range upstreams {
+		for _, u := range upstreams {
 			targetPath := strings.TrimSuffix(u.URL, "/") + r.URL.Path
 			c := client
 			if u.UseProxy {
@@ -51,20 +55,21 @@ func newHandle(httpProxy string, upstreams []upstream) (func(w http.ResponseWrit
 			}
 			_resp, err := c.Get(targetPath)
 			if err != nil {
-				log.Printf("  try upstream[%d]: %s, error: %s", i, targetPath, err)
+				log.Ctx(ctx).Debug().Err(err).Str("upstream", u.URL).Str("targetPath", targetPath).Msg("failed to get")
 				continue
 			}
 			if _resp.StatusCode != 200 {
-				log.Printf("  try upstream[%d]: %s, status code is: %d", i, targetPath, _resp.StatusCode)
+				log.Ctx(ctx).Debug().Str("upstream", u.URL).Str("targetPath", targetPath).Int("statusCode", _resp.StatusCode).Msg("failed to get")
 				_resp.Body.Close()
 				continue
 			}
-			log.Printf("  try upstream[%d]: %s, success", i, targetPath)
+			log.Ctx(ctx).Debug().Str("upstream", u.URL).Str("targetPath", targetPath).Msg("success to get")
 			resp = _resp
 			break
 		}
 		if resp == nil {
-			log.Printf("  all upstream not found")
+			// log.Printf("  all upstream not found")
+			log.Ctx(ctx).Debug().Msg("all upstream not found")
 			w.WriteHeader(404)
 			return
 		}
@@ -74,7 +79,7 @@ func newHandle(httpProxy string, upstreams []upstream) (func(w http.ResponseWrit
 		// write resp.Body to cachefile
 		cacheFile, err := os.Create(cacheFilePath)
 		if err != nil {
-			log.Fatal().Err(err).Msg("failed to create cache file") // TODO: 500
+			log.Ctx(ctx).Fatal().Err(err).Msg("failed to create cache file") // TODO: 500
 			return
 		}
 		defer cacheFile.Close()
@@ -82,7 +87,7 @@ func newHandle(httpProxy string, upstreams []upstream) (func(w http.ResponseWrit
 		// TODO: write to temp file first
 		_, err = io.Copy(cacheFile, resp.Body)
 		if err != nil {
-			log.Fatal().Err(err).Msg("failed to write cache file") // TODO: 500
+			log.Ctx(ctx).Fatal().Err(err).Msg("failed to write cache file") // TODO: 500
 			return
 		}
 
@@ -92,7 +97,7 @@ func newHandle(httpProxy string, upstreams []upstream) (func(w http.ResponseWrit
 }
 
 func main() {
-	goutils.InitZeroLog()
+	goutils.InitZeroLog(goutils.WithProduction{})
 	if err := os.MkdirAll(cacheDir, os.ModePerm); err != nil {
 		log.Fatal().Err(err).Msg("failed to create cache dir")
 	}
